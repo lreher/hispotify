272e91f43d188667765afc3e7c0c43643f2f2fe4
Lucas <lucas.reher@gmail.com>
2020-03-10 08:06:50 +1000

    added simple routing and serving

--- router/index.js
+++ router/index.js
@@ -1,3 +1,29 @@
+var fs = require('fs');
+var path = require('path');
+
 module.exports = function(request, response) {
-  console.log(request.url)
+  var url = request.url;
+  var servePath
+
+  switch(url) {
+    case '/':
+      servePath = path.resolve(__dirname, "../client/index.html")
+
+      response.writeHead(200, { 'Content-Type': 'text/html' })
+      fs.createReadStream(servePath, 'utf-8').pipe(response)
+
+      break;
+
+    case '/bundle.js':
+      servePath = path.resolve(__dirname, "../client/bundle.js")
+
+      response.writeHead(200, { 'Content-Type': 'text/json' })
+      fs.createReadStream(servePath, 'utf-8').pipe(response)
+
+      break;
+
+    default:
+      response.writeHead(400);
+      response.end("Bad Request.");
+  }
 }
