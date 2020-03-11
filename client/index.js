482d66b58d2afc4bd6b13d7d308cf17e53ae3358
Lucas <lucas.reher@gmail.com>
2020-03-12 08:23:13 +1000

    parse jsx with babel

--- client/index.js
+++ client/index.js
@@ -1,12 +1,16 @@
 const React = require('react');
 const ReactDOM = require('react-dom')
 
-const element = React.createElement(
-  'div',
-  {className: 'greeting'},
-  'Hello, world!'
-);
+// const element = React.createElement(
+//   'div',
+//   {className: 'greeting'},
+//   'Hello, world!'
+// );
+
+// BUT WHAT IF
+const element = <h1>Hey Buddy</h1>;
+
 
 document.addEventListener('DOMContentLoaded', function () {
-  ReactDOM.render(element, document.getElementById('myApp'))
+  ReactDOM.render(element, document.getElementById('root'))
 })
--- client/static/bundle.js
+++ client/static/bundle.js
@@ -1,16 +1,18 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const React = require('react');
-const ReactDOM = require('react-dom')
 
-const element = React.createElement(
-  'div',
-  {className: 'greeting'},
-  'Hello, world!'
-);
+const ReactDOM = require('react-dom'); // const element = React.createElement(
+//   'div',
+//   {className: 'greeting'},
+//   'Hello, world!'
+// );
+// BUT WHAT IF
 
+
+const element = React.createElement("h1", null, "Hey Buddy");
 document.addEventListener('DOMContentLoaded', function () {
-  ReactDOM.render(element, document.getElementById('myApp'))
-})
+  ReactDOM.render(element, document.getElementById('root'));
+});
 
 },{"react":10,"react-dom":7}],2:[function(require,module,exports){
 /*
